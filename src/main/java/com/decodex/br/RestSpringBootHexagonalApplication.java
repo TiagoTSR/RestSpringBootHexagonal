ed6ae988f53ebb15a966e1e302df6e84ac9840f6
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-04-17 16:09:52 -0300

    chore: cria estrutura inicial do projeto

---
+++ src/main/java/com/decodex/br/RestSpringBootHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.decodex.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/decodex/br/RestSpringBootHaxagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decodex.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootHaxagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
