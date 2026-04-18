9c94b4afd19ee8244751608bd0e8a2473e98e1e5
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-04-18 17:15:23 -0300

    refactor: instanciar BookMapper via construtor no  BookRepositoryAdapter

--- src/main/java/com/decodex/br/adapters/out/persistence/adapter/BookRepositoryAdapter.java
+++ src/main/java/com/decodex/br/adapters/out/persistence/adapter/BookRepositoryAdapter.java
@@ -11,34 +11,35 @@ import com.decodex.br.adapters.out.persistence.repository.BookJpaRepository;
 import com.decodex.br.domain.model.Book;
 import com.decodex.br.domain.port.out.BookRepositoryPort;
 
-
 @Component
 public class BookRepositoryAdapter implements BookRepositoryPort {
 
     private final BookJpaRepository repository;
+    private final BookMapper mapper;
 
     public BookRepositoryAdapter(BookJpaRepository repository) {
         this.repository = repository;
+        this.mapper = new BookMapper();
     }
 
     @Override
     public Book save(Book book) {
-        return BookMapper.toDomain(
-            repository.save(BookMapper.toEntity(book))
+        return mapper.toDomain(
+            repository.save(mapper.toEntity(book))
         );
     }
 
     @Override
     public Optional<Book> findById(Long id) {
         return repository.findById(id)
-                .map(BookMapper::toDomain);
+                .map(mapper::toDomain);
     }
 
     @Override
     public List<Book> findAll() {
         return repository.findAll()
                 .stream()
-                .map(BookMapper::toDomain)
+                .map(mapper::toDomain)
                 .collect(Collectors.toList());
     }
 
