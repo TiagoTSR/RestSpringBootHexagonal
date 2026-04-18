6eb600ca563a52bb4463797775e082b4da51cd0e
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-04-18 17:10:17 -0300

    feat: Adicionando migrations Flyway

---
+++ src/main/resources/db/migration/V02__Popular_Tabela_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO person (id, address, first_name, gender, last_name) VALUES
+    (1, 'São Paulo', 'Ayrton', 'MALE', 'Senna'),
+    (2, 'Anchiano - Italy', 'Leonardo', 'MALE', 'Da Vinci'),
+    (3, 'Jerusalém', 'Judas', 'MALE', 'Escariotes'),
+    (4, 'São Paulo', 'Raquel', 'FEMALE', 'Sherazade'),
+    (5, 'Anchiano dgegs455455segsegseg - Italy', 'Leonardo45', 'MALE', 'Da Vinci545'),
+    (6, 'Mvezo – South Africa', 'Nelson', 'MALE', 'Mandela'),
+    (7, 'Brasilia', 'Renan', 'MALE', 'Oliveira'),
+    (8, 'Bahia', 'Whinderson', 'MALE', 'Nunes'),
+    (9, 'Estados - unidos', 'Taylor', 'FEMALE', 'Swift');
